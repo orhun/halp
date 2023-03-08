ec40b14cdcb9f8a38715900847a8d91fe9991a0c
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-03-08 22:53:24 +0300

    style(prompt): customize the selection prompt colors

--- src/helper/docs/mod.rs
+++ src/helper/docs/mod.rs
@@ -7,7 +7,7 @@ pub mod cheat;
 use crate::error::Result;
 use crate::helper::docs::cheat::show_cheat_sheet;
 use crate::helper::docs::man::show_man_page;
-use console::Term;
+use console::{style, Style, Term};
 use dialoguer::theme::ColorfulTheme;
 use dialoguer::Select;
 use std::io::Write;
@@ -16,7 +16,7 @@ use std::io::Write;
 pub fn get_docs_help<Output: Write>(bin: &str, man_cmd: &str, output: &mut Output) -> Result<()> {
     let mut selection = Some(0);
     loop {
-        selection = Select::with_theme(&ColorfulTheme::default())
+        selection = Select::with_theme(&get_selection_theme())
             .with_prompt("Select operation")
             .default(selection.unwrap_or_default())
             .items(&["Show man page", "Show cheat sheet", "Exit"])
@@ -26,5 +26,30 @@ pub fn get_docs_help<Output: Write>(bin: &str, man_cmd: &str, output: &mut Outpu
             Some(1) => show_cheat_sheet(bin, output)?,
             _ => return Ok(()),
         };
+    }
+}
+
+/// Returns the theme for selection prompt.
+fn get_selection_theme() -> ColorfulTheme {
+    ColorfulTheme {
+        defaults_style: Style::new().for_stderr().cyan(),
+        prompt_style: Style::new().for_stderr().bold(),
+        prompt_prefix: style("(ﾉ´ヮ`)ﾉ*: ･ﾟ\n".to_string()).for_stderr().magenta(),
+        prompt_suffix: style("›".to_string()).for_stderr().magenta().bright(),
+        success_prefix: style("❤".to_string()).for_stderr().magenta(),
+        success_suffix: style("·".to_string()).for_stderr().black().bright(),
+        error_prefix: style("✘".to_string()).for_stderr().red(),
+        error_style: Style::new().for_stderr().red(),
+        hint_style: Style::new().for_stderr().black().bright(),
+        values_style: Style::new().for_stderr().green(),
+        active_item_style: Style::new().for_stderr().green().bold(),
+        inactive_item_style: Style::new().for_stderr().italic(),
+        active_item_prefix: style("✧".to_string()).for_stderr().magenta().bold(),
+        inactive_item_prefix: style(" ".to_string()).for_stderr(),
+        checked_item_prefix: style("❤".to_string()).for_stderr().magenta(),
+        unchecked_item_prefix: style("❤".to_string()).for_stderr().black(),
+        picked_item_prefix: style("❯".to_string()).for_stderr().green(),
+        unpicked_item_prefix: style(" ".to_string()).for_stderr(),
+        inline_selections: true,
     }
 }
