8f51bda443d45a507e2a0ebc4939e8a1e33759cc
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-03-08 19:45:49 +0300

    feat(prompt): support selection for additional help

--- src/helper/docs/mod.rs
+++ src/helper/docs/mod.rs
@@ -7,11 +7,21 @@ pub mod cheat;
 use crate::error::Result;
 use crate::helper::docs::cheat::show_cheat_sheet;
 use crate::helper::docs::man::show_man_page;
+use console::Term;
+use dialoguer::theme::ColorfulTheme;
+use dialoguer::Select;
 use std::io::Write;
 
 /// Shows documentation/usage help about the given binary.
 pub fn get_docs_help<Output: Write>(bin: &str, man_cmd: &str, output: &mut Output) -> Result<()> {
-    show_man_page(man_cmd, bin)?;
-    show_cheat_sheet(bin, output)?;
-    Ok(())
+    let operation = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Select operation")
+        .default(0)
+        .items(&["Show man page", "Show cheat sheet"])
+        .interact_on_opt(&Term::stderr())?;
+    match operation {
+        Some(0) => show_man_page(man_cmd, bin),
+        Some(1) => show_cheat_sheet(bin, output),
+        _ => Ok(()),
+    }
 }
