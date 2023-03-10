9bc47f55bb7793455158a8ae62cc27bc119cb8d7
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-03-10 03:40:22 +0300

    fix(tty): disable default shell as default

--- src/helper/tty.rs
+++ src/helper/tty.rs
@@ -12,7 +12,7 @@ impl TtyCommand {
     /// Creates a command with the default shell.
     #[allow(clippy::new_ret_no_self)]
     pub fn new(command: &str) -> Result<OsCommand> {
-        Self::new_with_shell(command, DEFAULT_SHELL)
+        Self::new_with_shell(command, None)
     }
 
     /// Creates a command that is executed by a shell, pretending to be a TTY.
@@ -27,17 +27,23 @@ impl TtyCommand {
     /// - [Apple docs](https://opensource.apple.com/source/shell_cmds/shell_cmds-170/script/script.1.auto.html)
     ///
     /// On Windows (which is unsupported), the command is returned as-is.
-    pub fn new_with_shell(cmd: &str, shell: &str) -> Result<OsCommand> {
+    pub fn new_with_shell(cmd: &str, shell: Option<&str>) -> Result<OsCommand> {
         if cfg!(any(target_os = "linux", target_os = "android")) {
             let mut command = OsCommand::new("script");
             command.args(["-q", "-e", "-c", cmd, "/dev/null"]);
-            command.env("SHELL", shell.trim());
-            println!("1: {:?}", command);
+            if let Some(shell) = shell {
+                command.env("SHELL", shell.trim());
+            }
             Ok(command)
         } else if cfg!(any(target_os = "macos", target_os = "freebsd")) {
             let mut command = OsCommand::new("script");
-            command.args(["-q", "/dev/null", shell.trim(), "-c", cmd]);
-            println!("2: {:?}", command);
+            command.args([
+                "-q",
+                "/dev/null",
+                shell.unwrap_or(DEFAULT_SHELL).trim(),
+                "-c",
+                cmd,
+            ]);
             Ok(command)
         } else if cfg!(target_os = "windows") {
             let mut command = OsCommand::new("cmd");
@@ -56,7 +62,7 @@ mod tests {
     use std::process::Stdio;
 
     fn run_command(cmd: &str, shell: Option<&str>) -> Result<String> {
-        let command = TtyCommand::new_with_shell(cmd, shell.unwrap_or(DEFAULT_SHELL))?
+        let command = TtyCommand::new_with_shell(cmd, shell)?
             .stderr(Stdio::inherit())
             .output()?;
         Ok(String::from_utf8_lossy(&command.stdout).replace("\r\n", "\n"))
